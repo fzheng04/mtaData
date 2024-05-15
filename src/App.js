700d5ad7aedc0f8f7c475556e98d8caa1ae7b19d
fei <feiyang91604@gmail.com>
2024-05-15 01:05:14 -0400

    input boxes

--- src/App.js
+++ src/App.js
@@ -2,22 +2,25 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div id = "body">
+      
+      <div id = "from">
+        from:
+        <input></input>
+        <button></button>
+      </div>
+      <div id = "to">
+        to:
+        <input></input>
+        <button></button>
+      </div>
+      
+        
+      
+      
     </div>
   );
 }
